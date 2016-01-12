e77d6d9f8b675035caa60574c888220249d5ae99
Carlos Genaro <hyunkeln@gmail.com>
2016-01-11 18:06:54 -0600

    Programa extra

---
+++ sesión1/saludar.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+# -*-encoding: utf-8 -*-
+
+print "Ingresa tu nombre: "
+name=STDIN.gets.chop!
+print "Ingresa tu edad: "
+age=STDIN.gets.chop!
+print "Hola ",name,", tienes ",age," años de edad\n"
