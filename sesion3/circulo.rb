6d2d934d36c339d88ef88bbcb709b56d9c977257
Carlos Genaro <hyunkeln@gmail.com>
2016-01-13 17:38:03 -0600

    Circulo funcionando

--- sesion3/circulo.rb
+++ sesion3/circulo.rb
@@ -1,30 +1,30 @@
 #!/usr/bin/ruby
 # -*-encoding: utf-8 -*-
 
-opcion = ""
+opcion = 0
 while opcion != "a" && opcion != "b" && opcion != "c" && opcion != "d"
     puts "Elige una opción: "
     puts "a) Calcular el diámetro"
     puts "b) Calcular el perímetro"
     puts "c) Calcular el área"
     puts "d) Finalizar"
-    opcion = STDIN.gets.chop
+    opcion = STDIN.gets.chomp
     
-    if opcion == "d" 
-        break 
-    elsif
+    if opcion != "d" 
         print "Introduce el radio: " 
-        radio = STDIN.gets.chop.to_f
-        
-        resultado = case opcion
-            when "a" then "El diámetro es 2*r: "+(2*radio)
-            when "b" then "El perímetro es pi*r: "+(MATH::PI*radio)
-            when "c" then "El área es pi*r^2: "+(MATH::PI*radio**2)
-            else "Opcion incorrecta: "
-            end
-        puts resultado
-        if opcion == "a" || opcion == "b" || opcion == "c" 
-            break
+        radio = STDIN.gets.chomp.to_f
+        case opcion
+            when "a" then
+                puts "El diámetro es 2*r: "+(2*radio).to_s
+                break
+            when "b" then
+                puts "El perímetro es pi*r: "+(Math::PI*radio).to_s
+                break
+            when "c" then
+                puts "El área es pi*r^2: "+(Math::PI*radio**2).to_s
+                break
+            else
+                puts "Opcion incorrecta: "
         end
     end
 end
