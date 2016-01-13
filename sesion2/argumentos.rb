80b1a88ba84fa9ef128f08653ee3dd15c787e216
Carlos Genaro <hyunkeln@gmail.com>
2016-01-12 18:37:29 -0600

    Manejo de argumentos

---
+++ sesion2/argumentos.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+# -*-encoding: utf-8 -*-
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+puts ARGV[0]
+puts ARGV[1]
+puts ARGV[2]
